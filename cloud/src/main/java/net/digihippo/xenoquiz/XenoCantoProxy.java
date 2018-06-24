770cd6348f8d7dd2aa3faf8127ac7b0df33946cd
James Byatt <grumpyjames@gmail.com>
2018-06-24 19:59:48 +0100

    Locally cache api requests, because the xeno canto api can be slow (means requests take ~20ms, not 500ms+)

--- cloud/src/main/java/net/digihippo/xenoquiz/XenoCantoProxy.java
+++ cloud/src/main/java/net/digihippo/xenoquiz/XenoCantoProxy.java
@@ -1,57 +1,64 @@
 package net.digihippo.xenoquiz;
 
-import javax.net.ssl.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @WebServlet(name = "XenoCantoProxy", value = "/api/birds")
 public class XenoCantoProxy extends HttpServlet {
+
+  private static final Map<String, String> cache = new ConcurrentHashMap<>();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
 
-    final String birdName = URLEncoder.encode(
-        request.getParameter("birdName"), StandardCharsets.UTF_8.name());
-    final String xenoApiUrl = "https://www.xeno-canto.org/api/2/recordings?query=" + birdName;
+    final String birdNameRaw = request.getParameter("birdName");
+    final String encodedName = URLEncoder.encode(
+        birdNameRaw, StandardCharsets.UTF_8.name());
 
-    final URL url = new URL(xenoApiUrl);
-    final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-    if (connection.getResponseCode() < 300)
+    if (cache.get(birdNameRaw) == null)
     {
-      response.setStatus(200);
-      response.setHeader("Content-Type", "application/json");
-      final byte[] buffer = new byte[1024];
-      try (
-          final InputStream inputStream = connection.getInputStream();
-          final OutputStream outputStream = response.getOutputStream()
-      )
-      {
-        int readCount = inputStream.read(buffer);
-        while (readCount != -1)
-        {
-          outputStream.write(buffer, 0, readCount);
+      System.out.println("Cache miss for " + birdNameRaw);
+      final String xenoApiUrl = "https://www.xeno-canto.org/api/2/recordings?query=" + encodedName;
+      final URL url = new URL(xenoApiUrl);
+      final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-          readCount = inputStream.read(buffer);
+      if (connection.getResponseCode() < 300)
+      {
+        final StringBuilder builder = new StringBuilder();
+        final byte[] buffer = new byte[1024];
+        try (final InputStream inputStream = connection.getInputStream())
+        {
+          int readCount = inputStream.read(buffer);
+          while (readCount != -1)
+          {
+            builder.append(new String(buffer, 0, readCount, StandardCharsets.UTF_8));
+
+            readCount = inputStream.read(buffer);
+          }
+
+          cache.put(birdNameRaw, builder.toString());
         }
       }
+      else
+      {
+        response.setStatus(connection.getResponseCode());
+      }
     }
-    else
-    {
-      response.setStatus(connection.getResponseCode());
-    }
+
+    response.setStatus(200);
+    response.setHeader("Content-Type", "application/json");
+    response.getOutputStream().print(cache.get(birdNameRaw));
+    response.getOutputStream().close();
   }
 }
