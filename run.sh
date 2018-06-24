ca598b64a4269dcbad4b8755f65ee16696589d6c
James Byatt <grumpyjames@gmail.com>
2018-06-24 19:01:03 +0100

    Two quick scripts to automate run and deploy, however clunky they might be

---
+++ run.sh
@@ -0,0 +1,13 @@
+WEB_OUT=cloud/src/main/webapp
+
+elm-make Main.elm --output $WEB_OUT/my.js
+
+cp index.html $WEB_OUT/
+cp styles.css $WEB_OUT/
+
+pushd cloud
+
+mvn -Dhttps.protocols=TLSv1.2 clean package
+mvn -Dhttps.protocols=TLSv1.2 appengine:devserver
+
+popd
